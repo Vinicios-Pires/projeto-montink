8c586ff0575027367b04202d336d41398be1cf96
Vinicios-Pires <contato.viniciospires@gmail.com>
2025-05-25 23:00:51 -0300

    feat: adding responsivity

--- src/components/infos-product/index.tsx
+++ src/components/infos-product/index.tsx
@@ -3,22 +3,22 @@ import { formatPrice } from "../../utils/fn";
 
 const InfosProduct = () => {
   return (
-    <div className="flex flex-col w-[20%]">
+    <div className="flex flex-col w-full lg:w-[20%] order-1 lg:order-1">
       <div className="flex flex-col gap-1">
-        <h2 className="text-[20px] leading-[1.4] text-custom-primary-text font-bold">
+        <h2 className="text-[18px] lg:text-[20px] leading-[1.4] text-custom-primary-text font-bold">
           {product.title}
         </h2>
-        <h2 className="text-[20px] leading-[1.4] text-custom-primary-text font-bold mb-4">
+        <h2 className="text-[18px] lg:text-[20px] leading-[1.4] text-custom-primary-text font-bold mb-4">
           {formatPrice(product.price)}
         </h2>
-        <p>
+        <p className="text-sm lg:text-base">
           <span className="font-semibold">Material:</span> {product.material}
         </p>
-        <p>
+        <p className="text-sm lg:text-base">
           <span className="font-semibold">Indicado para:</span>{" "}
           {product.indicatedFor}
         </p>
-        <p>
+        <p className="text-sm lg:text-base">
           <span className="font-semibold">Altura do cano:</span>{" "}
           {product.ankleHeight}
         </p>
